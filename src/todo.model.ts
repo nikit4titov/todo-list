ac30efcf70ff8163fb25c58591efa8571b4cb5f9
Nikita Titov <nikitatitov.main@gmail.com>
2021-08-13 14:42:06 +0300

    Refactor code

--- src/todo.model.ts
+++
@@ -1,8 +0,0 @@
-interface TodoModel {
-  id: number;
-  title: string;
-  completed: boolean;
-  editing: boolean;
-}
-
-export default TodoModel;
--- src/todos-storage.ts
+++ src/todos-storage.ts
@@ -1,13 +1,18 @@
-import TodoModel from "@/todo.model";
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+  editing: boolean;
+}
 
-const TODOS_STORAGE_KEY = "todos";
+const STORAGE_KEY = "todos";
 
 export default {
-  save(todos: TodoModel[]): void {
-    localStorage.setItem(TODOS_STORAGE_KEY, JSON.stringify(todos));
+  save(todos: Todo[]): void {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
   },
-  fetch(): TodoModel[] {
-    const todos = localStorage.getItem(TODOS_STORAGE_KEY);
+  fetch(): Todo[] {
+    const todos = localStorage.getItem(STORAGE_KEY);
     if (todos === null) {
       return [];
     } else {
